fe219719a0615297137702495bec637172b42125
MiaaaaaaZ <mirongzou@gmail.com>
2020-02-16 16:24:24 -0800

    service worker import

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 import reduxThunk from 'redux-thunk';
+import * as serviceWorker from './serviceWorker';
 
 import App from './components/App';
 import reducers from './reducers';
